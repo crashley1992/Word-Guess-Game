1f848f851397d88b80ca4160dd82c2a01f28be69
Ashley Clarke <crashley92@gmail.com>
2019-08-07 12:44:30 -0700

    styling for game

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,9 @@
 //var for game status so it doesn't run until user clicks start
 var start = false;
+//counter trackers
+var winner = 0;
+var correct = 0;
+var guess = 0;
 
 //global function to end game
 function stopGame() {
@@ -10,7 +14,7 @@ function stopGame() {
     start = true;
 
     //Words to guess from
-var words = ["help", "depression", "sorrow"];
+var words = ["help", "sad", "sorrow"];
 //picks random word from array
 var word = words[Math.floor(Math.random() * words.length)];
 
@@ -39,45 +43,42 @@ var letter = event.key.toString();
             remainingLetters--;
             document.getElementById("letter1").innerHTML = answerArray;
             //Word guessed correctly counter
-
-            var correct = 0;
             correct++;
             document.getElementById("letters-correct").innerHTML = correct;
 
             //ends guessing once all letters have been guesses. 
             if(remainingLetters === 0) {
                 console.log("winner");
-                var winner = 0;
-                 winner = winner + 1;
+                 winner++;
+                 //Ends Game when win counter hits 3.
+                 if(winner == 3) {
+                    console.log("You Won!")
+                    //write function that ends Game
+                 }
                  document.getElementById("win-tracker").innerHTML = winner;
+                
                 }
-         } else {
-            console.log("wrong");
-            // guess total - correct guesses
-            }
+            } 
          }     
    }
- 
 
-var guess = 0;
+
 //guess tracker
    document.addEventListener("keyup", function(event){
         guess++;
         console.log(guess);
         document.getElementById("guess-tracker").innerHTML = guess;
         //remaining guess counter 
-        var remaining = 20 - guess;
+        var remaining = 26 - guess;// at 26 because it makes up for enter button
         document.getElementById("guess-remaining").innerHTML = remaining;
         //end game conditional
-        if (guess > 20) {
-            console.log("you lose"); //display Game over box
+        if (guess > 26) {
+            document.getElementById("game-over").innerHTML = "Game Over"; //display Game over box
         }
    });
   }
 
 
-  //next queue word for game
-
-
-  //if words are all used up -- end game
+  //next queue word for same logic for next button as start game. 
+  //generates new word.
 
